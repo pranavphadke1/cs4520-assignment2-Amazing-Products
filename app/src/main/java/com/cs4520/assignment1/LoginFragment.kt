ed25243a5462ca48ecaac421c1009de1d18b6b31
Pranav Phadke <pranavmphadke@gmail.com>
2024-02-13 20:17:28 -0500

    Setting up login and productList fragments

--- app/src/main/java/com/cs4520/assignment1/LoginFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/LoginFragment.kt
@@ -1,6 +1,24 @@
 package com.cs4520.assignment1
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class LoginFragment:Fragment(R.layout.login_view) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.login_view, container, false)
+        val loginButton = view.findViewById<View>(R.id.login_button)
+        val navController = findNavController()
+        loginButton.setOnClickListener {
+            navController.navigate(R.id.action_loginFragment_to_productListFragment)
+        }
+        return view
+    }
 }
--- app/src/main/java/com/cs4520/assignment1/ProductListFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/ProductListFragment.kt
@@ -1,6 +1,24 @@
 package com.cs4520.assignment1
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class ProductListFragment:Fragment(R.layout.product_view) {
+class ProductListFragment:Fragment(R.layout.product_list_view) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.product_list_view, container, false)
+        val products = productsDataset.map { Product(it) }
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+//        recyclerView.adapter = products
+        return view
+    }
 }
